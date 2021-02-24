fd717e406d3367ec32221984b70c39387e4e90c6
paul brown <paul@whatstherub.com>
2021-02-24 08:50:52 -1000

    feat: cache policy support

--- main.tf
+++ main.tf
@@ -142,9 +142,9 @@ resource "aws_cloudfront_distribution" "default" {
     # }
 
     viewer_protocol_policy = var.viewer_protocol_policy
-    default_ttl            = var.default_ttl
-    min_ttl                = var.min_ttl
-    max_ttl                = var.max_ttl
+    # default_ttl            = var.default_ttl
+    # min_ttl                = var.min_ttl
+    # max_ttl                = var.max_ttl
   }
 
   dynamic "ordered_cache_behavior" {
